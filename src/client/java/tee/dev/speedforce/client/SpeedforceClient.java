dcd992ee163bdc406f01d1e567e08eac219793ee
tay <tay@havenmc.org>
2024-09-19 00:15:42 -0400

    Initial commit

---
+++ src/client/java/tee/dev/speedforce/client/SpeedforceClient.java
@@ -0,0 +1,10 @@
+package tee.dev.speedforce.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpeedforceClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/tee/dev/speedforce/Speedforce.java
@@ -0,0 +1,10 @@
+package tee.dev.speedforce;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Speedforce implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
